e094c990d8372a6e1223186244ec7773fe6e41a7
dingyujie <2946803216@qq.com>
2022-08-03 22:07:39 +0800

    实现界面demo样式布局

--- src/drag.jsx
+++ src/drag.jsx
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './drag.less';
+import addImg from './static/icon/add.png';
+import delImg from './static/icon/delete.png';
 
 const Drag = () => {
   const [landing, setLanding] = useState(true); // landing状态
   const [prepareList, setPrepareList] = useState([]);
   const [learningList, setLearningList] = useState([]);
   const [completeList, setCompleteList] = useState([]);
+  const [showInput, setShowInput] = useState(false); // 是否显示新增输入框
   const [showDel, setShowDel] = useState(false); // 是否显示删除按钮
   const [currentTask, setCurrentTask] = useState({}); // 当前选中的任务
 
   useEffect(() => {
+    console.log('prepareList：', prepareList);
+  }, [prepareList])
 
-  }, [])
+  const addTask = (e) => {
+    console.log(e.target.value);
+    let curValue = e.target.value;
+    if (curValue) {
+      setPrepareList([...prepareList, curValue])
+    }
+    setShowInput(false);
+  }
 
   //鼠标华划过接受拖拽元素的事件
   const handleDrop = (callBack, e, arrow) => {
@@ -44,13 +56,30 @@ const Drag = () => {
   const handleDragLeave = e => e.target.classList.remove('over')
 
   return (
-    <div className='main'>
+    <div className="main">
       {
-        landing ? (<div className='start'>
-          <button className='button'>Start App</button>
-        </div>) : (
-          <div>111</div>
-        )
+        landing ?
+          (<div className='start'>
+            <button className='button' onClick={() => { console.log('点击start'); setLanding(false) }}>Start App</button>
+          </div>) :
+          (<div className='container'>
+            <div className='left'>
+              <div className='title'>Prepare to study</div>
+              <div className='content'>
+                <div>prepareList</div>
+                <input style={{ display: showInput ? 'inline-block' : 'none' }} onKeyDown={(e) => { if (e.keyCode === 13) { addTask(e) } }}></input>
+                <div className='icon' onClick={() => { setShowInput(true) }}><img src={addImg} alt='add task' /></div>
+              </div>
+            </div>
+            <div className='center'>
+              <div className='title'>Learning</div>
+              <div className='content'>prepareList</div>
+            </div>
+            <div className='right'>
+              <div className='title'>Complete</div>
+              <div className='content'>prepareList</div>
+            </div>
+          </div>)
       }
     </div>
   )
